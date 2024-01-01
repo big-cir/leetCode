fa00bd856767e44f580dc1084f546684d3095a4b
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-01-01 21:43:13 +0900

    Time: 578 ms (5.26%), Space: 44.5 MB (16.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
-                }
+        int[] answer = new int[2];
+        back_tracking(0, 0, new int[] {-1, -1}, answer, nums, target);
+        return answer;
+    }
+    
+    private void back_tracking(int level, int startIdx, int[] tmp, int[] answer, int[] nums, int target) {
+        if (level == 2) {
+            if (nums[tmp[0]] + nums[tmp[1]] == target) {
+                answer[0] = tmp[0];
+                answer[1] = tmp[1];
             }
+            return;
         }
-        return new int[] {0, 0};
+        
+        for (int i = startIdx; i < nums.length; i++) {
+            tmp[level] = i;
+            back_tracking(level + 1, i + 1, tmp, answer, nums, target);
+            tmp[level] = -1;
+        }
     }
 }
