0abde95b6002a6b22c5e417d418833bcd818ef70
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-01-02 19:25:07 +0900

    Time: 250 ms (19.06%), Space: 41.3 MB (35.22%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,42 +1,37 @@
 class Solution {
-    static boolean answer;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     
     public boolean exist(char[][] board, String word) {
-        answer = false;
-        
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == word.charAt(0)) {
-                    int[][] ch = new int[board.length][board[0].length];
-                    ch[i][j] = 1;
-                    dfs(board, word, i, j, 1, ch);
+                    char buffer = board[i][j];
+                    board[i][j] = '.';
+                    if (backtrack(board, i, j, word, 0)) return true;
+                    board[i][j] = buffer;
                 }
-                
-                if (answer) break;
             }
         }
-        return answer;
+        return false;
     }
     
-    private void dfs(char[][] board, String word, int x, int y, int idx, int[][] ch) {
-        if (idx == word.length()) {
-            answer = true;
-            return;
-        }
-        
+    private boolean backtrack(char[][] board, int x, int y, String word, int count) {
+        if (count + 1 == word.length()) return true;
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             
             if (nx >= 0 && nx < board.length && ny >= 0 && ny < board[0].length) {
-                if (board[nx][ny] == word.charAt(idx) && ch[nx][ny] == 0) {
-                    ch[nx][ny] = 1;
-                    dfs(board, word, nx, ny, idx + 1, ch);
-                    ch[nx][ny] = 0;
+                if (board[nx][ny] == word.charAt(count + 1)) {
+                    char buffer = board[nx][ny];
+                    board[nx][ny] = '.';
+                    if (backtrack(board, nx, ny, word, count + 1)) return true;
+                    board[nx][ny] = buffer;
                 }
             }
         }
+        
+        return false;
     }
 }
