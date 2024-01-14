61a394be5dbb439e3eac8593fbc3dc5848b6fcf1
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-01-14 19:00:52 +0900

    Time: 3 ms (15.22%), Space: 44.6 MB (69.21%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,18 +1,28 @@
 class Solution {
+    public class Element {
+        private int idx;
+        private int height;
+
+        public Element(int idx, int height) {
+            this.idx = idx;
+            this.height = height;
+        }
+    }
+    
     public int trap(int[] height) {
         int answer = 0;
-        Deque<Integer> stack = new ArrayDeque<>();
-        
+        Deque<Element> stack = new ArrayDeque<>();
+
         for (int i = 0; i < height.length; i++) {
-            while (!stack.isEmpty() && height[stack.peek()] < height[i]) {
-                int idx = stack.pop();
-                
-                if (stack.isEmpty()) break;
-                int gapIdx = i - stack.peek() - 1;
-                int rain = Math.min(height[i], height[stack.peek()]) - height[idx];
-                answer += (gapIdx * rain);
-            }
-            stack.push(i);
+            while (!stack.isEmpty() && height[i] > stack.peek().height) {
+                 Element element = stack.pop();
+                 if (stack.isEmpty()) break;
+
+                 int waterWidth = i - stack.peek().idx - 1;
+                 int waterHeight = Math.min(height[i], stack.peek().height) - height[element.idx];
+                 answer += waterWidth * waterHeight;
+            }       
+            stack.push(new Element(i, height[i]));
         }
         return answer;
     }
