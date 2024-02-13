60a71f6d7fa62452e16468a0972c5ce5ec7f05d7
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-02-13 12:20:32 +0900

    Time: 1153 ms (5.04%), Space: 146.6 MB (6.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,40 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] tmp = nums.clone();
-        int[] values = new int[2];
-
-        Arrays.sort(tmp);
-        int lt = 0;
-        int rt = tmp.length - 1;
-
-        while (lt < rt) {
-            int sum = tmp[lt] + tmp[rt];
-
-            if (sum <= target) {
-                if (sum == target) {
-                    values[0] = tmp[lt];
-                    values[1] = tmp[rt];
-                }
-                lt++;
-            } else {
-                rt--;
-            }
-        }
-
-        int idx1 = 0;
-        int idx2 = 0;
+        Map<Integer, int[]> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == values[0]) {
-                idx1 = i;
-                break;
+            for (int j = i + 1; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
+                map.put(sum, new int[] {i, j});
             }
         }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == values[1] && idx1 != i) {
-                idx2 = i;
-            }
-        }
-        return new int[] {idx1, idx2};
+        return map.get(target);
     }
 }
