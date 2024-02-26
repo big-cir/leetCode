70884c56394eb5bca6fb9c7d1358a6804d6777d8
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-02-26 19:28:16 +0900

    Time: 9 ms (95.57%), Space: 48.7 MB (42.15%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,56 +1,48 @@
 class Solution {
-    
     static class Node implements Comparable<Node> {
-        int v;
-        int cost;
-
-        public Node(int v, int cost) {
-            this.v = v;
+        private int vertex;
+        private int cost;
+        
+        public Node(int vertex, int cost) {
+            this.vertex = vertex;
             this.cost = cost;
         }
-
+        
         @Override
         public int compareTo(Node o) {
-            return cost - o.cost;
+            return this.cost - o.cost;
         }
     }
     
     public int networkDelayTime(int[][] times, int n, int k) {
-List<List<Node>> list = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            list.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < times.length; i++) {
-            int[] time = times[i];
-            list.get(time[0]).add(new Node(time[1], time[2]));
-        }
-
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+        List<List<Node>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
+        for (int i = 0; i < times.length; i++) graph.get(times[i][0]).add(new Node(times[i][1], times[i][2]));
+        
+        int[] distance = new int[n + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[k] = 0;
+        
+        Queue<Node> pq = new PriorityQueue<>();
         pq.offer(new Node(k, 0));
         
-        int[] dis = new int[n + 1];
-        Arrays.fill(dis, Integer.MAX_VALUE);
-        dis[k] = 0;
         while (!pq.isEmpty()) {
             Node now = pq.poll();
             
-            if (now.cost > dis[now.v]) continue;
-
-            for (Node next : list.get(now.v)) {
-                if (dis[next.v] > now.cost + next.cost) {
-                    dis[next.v] = now.cost + next.cost;
-                    pq.offer(new Node(next.v, now.cost + next.cost));
+            for (Node next : graph.get(now.vertex)) {
+                if (distance[next.vertex] > now.cost + next.cost) {
+                    distance[next.vertex] = now.cost + next.cost;
+                    pq.offer(new Node(next.vertex, now.cost + next.cost));
                 }
             }
         }
-
-        int answer = Integer.MIN_VALUE;
+        
+        int answer = 0;
         for (int i = 1; i <= n; i++) {
-            if (dis[i] == Integer.MAX_VALUE) return -1;
-            answer = Math.max(answer, dis[i]);
+            if (distance[i] == Integer.MAX_VALUE) return -1;
+            answer = Math.max(distance[i], answer);
         }
-
+        
         return answer;
     }
 }
