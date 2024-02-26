58a680639efdbc9978dfc4070401057ece0dcfa3
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-02-26 15:42:01 +0900

    Time: 5 ms (47.12%), Space: 44.6 MB (77.17%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,56 +1,57 @@
 class Solution {
+    static int[] dx = {-1, -1, 1, 1};
+    static int[] dy = {-1, 1, 1, -1};
+    static int[][] map;
+    
     public List<List<String>> solveNQueens(int n) {
-        String[][] ch = new String[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(ch[i], ".");
-        }
-
+        map = new int[n][n];
         List<List<String>> answer = new ArrayList<>();
+        int[][] ch = new int[n][n];
         backtrack(0, n, ch, answer);
-        return answer;
+        return answer;    
     }
     
-    private void backtrack(int level, int n, String[][] ch, List<List<String>> answer) {
-        if (level == n) {
-            List<String> sub = new ArrayList<>();
-            for (String[] row : ch) {
+    private void backtrack(int cnt, int n, int[][] ch, List<List<String>> answer) {
+        if (cnt == n) {
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
                 String tmp = "";
-                for (String x : row) tmp += x;
-                sub.add(tmp);
+                for (int j = 0; j < n; j++) {
+                    if (map[i][j] == 0) tmp += ".";
+                    else tmp += "Q";
+                }
+                list.add(tmp);
             }
-            answer.add(sub);
+            answer.add(list);
             return;
         }
-
-        String queen = "Q";
+        
         for (int i = 0; i < n; i++) {
-            if (validate(level, i, n, queen, ch)) {
-                ch[level][i] = queen;
-                backtrack(level + 1, n, ch, answer);
-                ch[level][i] = ".";
+            if (check(n, cnt, i)) {
+                map[cnt][i] = 1;
+                backtrack(cnt + 1, n, ch, answer);
+                map[cnt][i] = 0;
             }
         }
     }
-
-    private boolean validate(int x, int y, int n, String queen, String[][] ch) {
+    
+    private boolean check(int n, int x, int y) {
         for (int i = 0; i < n; i++) {
-            if (ch[x][i].equals(queen)) return false;
-            if (ch[i][y].equals(queen)) return false;
+            if (map[x][i] == 1) return false;
+            if (map[i][y] == 1) return false;
         }
-
-        int[] dx = {-1, -1, 1, 1};
-        int[] dy = {-1, 1, 1, -1};
+        
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-
+            
             while (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                if (ch[nx][ny].equals(queen)) return false;
+                if (map[nx][ny] == 1) return false;
                 nx += dx[i];
                 ny += dy[i];
             }
         }
-
+        
         return true;
     }
 }
