ff49bf0fc54561a55799ace5d9f3570952017f62
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-02-26 17:01:32 +0900

    Time: 5 ms (72.03%), Space: 44.8 MB (55.89%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -2,36 +2,32 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) graph.add(new ArrayList<>());
-
+        
         int[] indegree = new int[numCourses];
         for (int i = 0; i < prerequisites.length; i++) {
-            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
-            indegree[prerequisites[i][0]]++;
+            int[] info = prerequisites[i];
+            
+            indegree[info[1]]++;
+            graph.get(info[0]).add(info[1]);
         }
-
-        int[] ch = new int[numCourses];
+        
         Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < indegree.length; i++) {
+        for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) queue.offer(i);
         }
-
+        
+        int[] visit = new int[numCourses];
         while (!queue.isEmpty()) {
             int now = queue.poll();
-
-            if (ch[now] == 0) {
-                ch[now] = 1;
-
-                for (int next : graph.get(now)) {
-                    indegree[next]--;
-
-                    if (indegree[next] == 0) {
-                        queue.offer(next);
-                    }
-                }
+            visit[now] = 1;
+            
+            for (int next : graph.get(now)) {
+                indegree[next]--;
+                if (indegree[next] == 0) queue.offer(next);
             }
         }
-
-        for (int x : ch) {
+        
+        for (int x : visit) {
             if (x == 0) return false;
         }
         return true;
