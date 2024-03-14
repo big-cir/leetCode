c4ad482e211aac6afef88cef7736f7558379109f
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-03-14 13:12:38 +0900

    Time: 3 ms (30.46%), Space: 44.5 MB (15.66%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,68 +1,25 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-//         int[] ch = new int[rooms.size()];
-//         ch[0] = 1;
-//         boolean answer = true;
-//         for (int i = 0; i < rooms.size(); i++) {
-//             if (ch[i] == 1) {
-//                 ch[i] = 0;
-                
-//                 List<Integer> now = rooms.get(i);
-//                 for (int x : now) {
-//                     ch[x] = 1;
-//                 }
-//             } else {
-//                 answer = false;
-//             }
-             
-//         }
-//         return answer;
-        
-        // return bfs(rooms);
-        
         int[] visit = new int[rooms.size()];
-        dfs(0, rooms, visit);
-        for (int x : visit) {
-            if (x == 0) return false;
-        }
-        return true;
-    }
-    
-    public void dfs(int start, List<List<Integer>> rooms, int[] visit) {
-        visit[start] = 1;
-        for (int next : rooms.get(start)) {
-            if (visit[next] == 0) {
-                visit[next] = 1;
-                dfs(next, rooms, visit);
-            }
-        }
-    }
-    
-    private boolean bfs(List<List<Integer>> rooms) {
-        boolean flag = true;
-        int[] visit = new int[rooms.size()];
-        Queue<Integer> queue = new LinkedList<>();
         visit[0] = 1;
-        queue.offer(0);
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for (int x : rooms.get(0)) queue.offer(x);
         
         while (!queue.isEmpty()) {
-            int nowRoom = queue.poll();
-            
-            for (int next : rooms.get(nowRoom)) {
+            int now = queue.poll();
+            visit[now] = 1;
+            for (int next : rooms.get(now)) {
                 if (visit[next] == 0) {
-                    visit[next] = 1;
                     queue.offer(next);
                 }
             }
         }
         
         for (int x : visit) {
-            if (x == 0) {
-                flag = false;
-                break;
-            }
+            if (x == 0) return false;
         }
         
-        return flag;
+        return true;
     }
 }
