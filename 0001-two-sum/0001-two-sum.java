3162876b21c11a99866d0c9d703010695a47a42b
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-03-22 20:59:31 +0900

    Time: 2 ms (97.88%), Space: 44.9 MB (29.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,36 +2,17 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] answer = new int[2];
 
-        // {value, index}
-        List<int[]> list = new ArrayList<>();
+        // index, value
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            list.add(new int[] {nums[i], i});
-        }
-
-        Collections.sort(list, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
-            }
-        });
-
-        int lt = 0;
-        int rt = nums.length - 1;
-        while (lt < rt) {
-            int[] prev = list.get(lt);
-            int[] now = list.get(rt);
-
-            int sum = prev[0] + now[0];
-            if (sum <= target) {
-                if (sum == target) {
-                    answer[0] = prev[1];
-                    answer[1] = now[1];
-                    break;
-                }
-                lt++;
-            }
-            else {
-                rt--;
+            int gap = target - nums[i];
+            
+            if (map.containsKey(gap)) {
+                answer[0] = map.get(gap);
+                answer[1] = i;
+                return answer;
+            } else {
+                map.put(nums[i], i);
             }
         }
         return answer;
