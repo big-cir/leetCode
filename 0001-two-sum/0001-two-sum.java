c887622e3948f0da53b926cc105525d11f726a5f
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-03-22 20:52:49 +0900

    Time: 10 ms (54.23%), Space: 44.1 MB (99.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,39 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) { 
-        //✅ 숫자와 숫자의 인덱스를 키, 밸류로 하는 빈 해시테이블을 만든다.
-        Map<Integer, Integer> memo = new HashMap<>();
-        //✅ 숫자들을 순회한다.
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+
+        // {value, index}
+        List<int[]> list = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-		        //✅ 목표값을 만들기 위한 나머지 숫자를 구한다.
-            int needed = target - nums[i];
-		        //✅ 나머지 숫자가 해시테이블에 존재하면 그 수의 인덱스와 현재 인덱스를 반환한다.
-            if (memo.containsKey(needed)) {
-                return new int[] {memo.get(needed), i};
-            }
-		        //✅ 아니라면 해시테이블에 숫자와 인덱스를 추가한다.
-            memo.put(nums[i], i);
+            list.add(new int[] {nums[i], i});
         }
-        return new int[] {-1, -1};
+
+        Collections.sort(list, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int lt = 0;
+        int rt = nums.length - 1;
+        while (lt < rt) {
+            int[] prev = list.get(lt);
+            int[] now = list.get(rt);
+
+            int sum = prev[0] + now[0];
+            if (sum <= target) {
+                if (sum == target) {
+                    answer[0] = prev[1];
+                    answer[1] = now[1];
+                    break;
+                }
+                lt++;
+            }
+            else {
+                rt--;
+            }
+        }
+        return answer;
     }
 }
