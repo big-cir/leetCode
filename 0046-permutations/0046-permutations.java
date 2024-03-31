70d4317c796822a7b92831aef3f38195beee216f
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-03-31 23:26:26 +0900

    Time: 1 ms (95.58%), Space: 44.2 MB (76.88%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,28 @@
 class Solution {
+    static List<List<Integer>> answer;
+    static int[] ch;
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> answer = new ArrayList<>();
-        backtrack(nums, new int[nums.length], new ArrayList<>(), answer);
+        answer = new ArrayList<>();
+        ch = new int[nums.length];
+        backtrack(new ArrayList<>(), nums);
         return answer;
     }
     
-    private void backtrack(int[] nums, int[] ch, List<Integer> path, List<List<Integer>> answer) {
-        if (path.size() == nums.length) {
-            answer.add(new ArrayList<>(path));
+    private void backtrack(List<Integer> list, int[] nums) {
+        if (list.size() == nums.length) {
+            answer.add(new ArrayList<>(list));
             return;
         }
         
         for (int i = 0; i < nums.length; i++) {
             if (ch[i] == 0) {
                 ch[i] = 1;
-                path.add(nums[i]);
-                backtrack(nums, ch, path, answer);
-                path.remove(path.size() - 1);
+                list.add(nums[i]);
+                backtrack(list, nums);
+                list.remove(Integer.valueOf(nums[i]));
                 ch[i] = 0;
             }
+            
         }
     }
 }
