e6d822221e8383c0ec43b3013d80737376c55886
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-04-03 21:58:54 +0900

    Time: 3 ms (61.07%), Space: 43.3 MB (14.94%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,20 +1,20 @@
 class Solution {
     public int longestValidParentheses(String s) {
+        if (s.length() == 0) return 0;
         int answer = 0;
         Deque<Integer> stack = new ArrayDeque<>();
-
         stack.push(-1);
+
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ')') {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
                 stack.pop();
                 if (!stack.isEmpty()) {
                     answer = Math.max(answer, i - stack.peek());
                 } else {
                     stack.push(i);
                 }
-
-            } else {
-                stack.push(i);
             }
         }
         return answer;
