6e8734f859b95388bb086695f143f2e0f4c55bd4
dae won <99483390+big-cir@users.noreply.github.com>
2024-06-14 16:21:30 +0900

    Time: 678 ms (5.01%), Space: 46.3 MB (28.16%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,34 +1,21 @@
 class Solution {
-    static class Info {
-        private int idx;
-        private int h;
-
-        public Info(int idx, int h) {
-            this.idx = idx;
-            this.h = h;
-        }
-    }
-    
     public int trap(int[] height) {
         int answer = 0;
-        Deque<Info> stack = new ArrayDeque<>();
-        for (int i = 0; i < height.length; i++) {
-            while (!stack.isEmpty()) {
-                int top_Height = stack.peek().h;
-                int now_Height = height[i];
-                if (top_Height < now_Height) {
-                    Info top = stack.pop();
-                    if (stack.isEmpty()) break;
-                    int dis = i - stack.peek().idx - 1;
-
-                    int h = Math.min(height[i], stack.peek().h) - height[top.idx];
-                    answer += dis * h;
-                } else {
-                    break;
-                }
+        for (int i = 1; i < height.length; i++) {
+            int left = height[i];
+            for (int j = 0; j < i; j++) {
+                left = Math.max(left, height[j]);
             }
-            stack.push(new Info(i, height[i]));
+            
+            int right = height[i];
+            for (int j = i + 1; j < height.length; j++) {
+                right = Math.max(right, height[j]);
+            }
+            
+            int min = Math.min(left, right);
+            answer += Math.abs(min - height[i]);
         }
+        
         return answer;
     }
 }
