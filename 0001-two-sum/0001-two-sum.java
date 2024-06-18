b9ed496b0683ab16ca2859b4857bc19f2df5e18c
dae won <99483390+big-cir@users.noreply.github.com>
2024-06-18 20:09:06 +0900

    Time: 2 ms (97.80%), Space: 45 MB (12.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,17 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) { 
-        Map<Integer, Integer> memo = new HashMap<>();
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> store = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int needed = target - nums[i];
-		    
-            if (memo.containsKey(needed)) {
-                return new int[] {memo.get(needed), i};
+            int gap = target - nums[i];
+
+            if (store.containsKey(gap)) {
+                return new int[] {store.get(gap), i};
             }
-            memo.put(nums[i], i);
+
+            // 현재 값과 index를 저장
+            store.put(nums[i], i);
         }
+
         return new int[] {-1, -1};
     }
 }
