4176509c451404ee84fcc36b125b55a5f62d2282
dae won <99483390+big-cir@users.noreply.github.com>
2024-06-21 20:55:48 +0900

    Time: 2 ms (55.34%), Space: 41.4 MB (55.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,21 @@
+import java.util.*;
+
+
+// {()}
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
+        Stack<Character> stack = new Stack<>();
         for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch =='[') stack.push(ch);
-            else {
+            if (ch == '(' || ch == '{' || ch == '[') stack.push(ch);
+            else{
                 if (stack.isEmpty()) return false;
-                if (ch == ')') {
-                    if (stack.peek() != '(') return false;
-                    stack.pop();
-                }
-                else if (ch == '}') {
-                    if (stack.peek() != '{') return false;
-                    stack.pop();
-                }
-                else if (ch == ']') {
-                    if (stack.peek() != '[') return false;
-                    stack.pop();
-                }
+                
+                if (ch == ')' && stack.pop() != '(') return false;
+                if (ch == ']' && stack.pop() != '[') return false;
+                if (ch == '}' && stack.pop() != '{') return false;
             }
         }
-
+        
         return stack.isEmpty();
     }
 }
