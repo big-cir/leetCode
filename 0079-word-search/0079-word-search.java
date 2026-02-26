f45d695fc4f45948007f213ebabe622451097a44
dae won <99483390+big-cir@users.noreply.github.com>
2026-02-26 15:55:07 +0900

    Time: 139 ms (53.69%), Space: 43.1 MB (45.4%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,44 +1,41 @@
 class Solution {
+
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static boolean answer;
-    
+
     public boolean exist(char[][] board, String word) {
-        answer = false;
-        int n = board.length;
-        int m = board[0].length;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    int[][] ch = new int[n][m];
-                    ch[i][j] = 1;
-                    dfs(1, board, ch, i, j, word);
+        char start = word.charAt(0);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != start) continue;
+                if (dfs(i, j, 1, word, board)) {
+                    return true;
                 }
-                
-                if (answer) return answer;
             }
         }
-        return answer;
+
+        return false;
     }
-    
-    public void dfs(int level, char[][] board, int[][] ch, int x, int y, String s) {
-        if (level == s.length()) {
-            answer = true;
-            return;
-        }
-        
+
+    private boolean dfs(int x, int y, int index, String word, char[][] board) {
+        if (index == word.length()) return true;
+
+        char temp = board[x][y];
+        board[x][y] = '#';
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            
-            if (nx >= 0 && nx < board.length && ny >= 0 && ny < board[0].length && ch[nx][ny] == 0) {
-                if (board[nx][ny] == s.charAt(level)) {
-                    ch[nx][ny] = 1;
-                    dfs(level + 1, board, ch, nx, ny, s);
-                    ch[nx][ny] = 0;
+
+            if (nx >= 0 && nx < board.length && ny >= 0 && ny < board[0].length) {
+                if (board[nx][ny] == word.charAt(index)) {
+                    if (dfs(nx, ny, index + 1, word, board)) {
+                        return true;
+                    }
                 }
             }
         }
+
+        board[x][y] = temp;
+        return false;
     }
 }
